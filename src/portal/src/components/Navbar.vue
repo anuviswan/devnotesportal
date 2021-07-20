54defcf0ff7bcbd686d91124621a777f720ed19b
Anu Viswan <anu.viswan@gmail.com>
2021-07-20 21:15:42 +0530

    Implementing skeleton with vuetify

---
+++ src/portal/src/components/Navbar.vue
@@ -0,0 +1,13 @@
+<template>
+  <v-app-bar color="blue" dense dark app>
+    <div>Developer Sketch Notes</div>
+  </v-app-bar>
+</template>
+
+<script>
+export default {
+  name: "Navbar",
+};
+</script>
+
+<style></style>
