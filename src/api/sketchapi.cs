bcaa42790586a88446124f0ebc78ae4aac364555
Anu Viswan <anu.viswan@gmail.com>
2021-08-01 07:40:18 +0530

    Added getTags

--- src/api/sketchapi.cs
+++ src/api/sketchapi.cs
@@ -1,17 +1,12 @@
-using System;
-using System.IO;
-using System.Threading.Tasks;
-using Microsoft.AspNetCore.Mvc;
-using Microsoft.Azure.WebJobs;
-using Microsoft.Azure.WebJobs.Extensions.Http;
 using Microsoft.AspNetCore.Http;
-using Microsoft.Extensions.Logging;
-using Newtonsoft.Json;
+using Microsoft.AspNetCore.Mvc;
 //using Microsoft.WindowsAzure.Storage.Table;
 using Microsoft.Azure.Cosmos.Table;
-using System.Collections.Generic;
+using Microsoft.Azure.WebJobs;
+using Microsoft.Azure.WebJobs.Extensions.Http;
+using Microsoft.Extensions.Logging;
 using System.Linq;
-using System.Diagnostics;
+using System.Threading.Tasks;
 
 namespace api
 {
@@ -24,8 +19,8 @@ namespace api
             ILogger log)
         {
             TableContinuationToken continuationToken = null;
-            string queryString = TableQuery.GenerateFilterCondition("PartitionKey", QueryComparisons.Equal, "Tag");
-            var fluentQuery = new TableQuery<ImageEntry>().Where(queryString);
+            string queryString = TableQuery.GenerateFilterCondition("PartitionKey", QueryComparisons.Equal, TagEntry.PartitionTitle);
+            var fluentQuery = new TableQuery<TagEntry>().Where(queryString);
             var result = await table.ExecuteQuerySegmentedAsync(fluentQuery, continuationToken);
 
             return new OkObjectResult(result.Select(x=>x.Title));
@@ -36,10 +31,17 @@ namespace api
 
     public class ImageEntry:TableEntity
     {
+        public const string PartitionTitle = "Entry";
         public string Title { get; set; }
         public string Description { get; set; }
         public string Url { get; set; }
         public string Tags { get;set; }
 
+    }
+
+    public class TagEntry : TableEntity
+    {
+        public const string PartitionTitle = "Tag";
+        public string Title { get; set; }
     }
 }
